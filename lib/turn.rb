1875d1c2d2316f77e0b4dbb0495a745305e1248b
Jered Sweeney <sweeneyjered@gmail.com>
2017-10-07 17:36:34 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,11 +25,11 @@ def move(board, index, value = "X")
   board[index] = value
 end
 def input_to_index(input)
-  index = "#{input}".to_i - 1
+  "#{input}".to_i - 1
 end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  input_to_index(input)
-  puts index
+  index = input_to_index(input)
+  
 end
